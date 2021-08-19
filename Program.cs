03db33689af26aa5f944b587bc30c2f52e2d2525
Kurai Antal <antal.kurai@fluid.contitech.hu>
2021-08-19 17:15:31 +0200

    first example

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace linq
+class LINQQueryExpressions
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+
+        // Specify the data source.
+        int[] scores = new int[] { 97, 92, 81, 60 };
+
+        // Define the query expression.
+        IEnumerable<int> scoreQuery =
+            from score in scores
+            where score > 80
+            select score;
+
+        // Execute the query.
+        foreach (int i in scoreQuery)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write(i + " ");
         }
     }
 }
+// Output: 97 92 81
